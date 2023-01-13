5bfc021a89c09dd2fa993cf59919be7b57ba793f
Aulia Rasyid <aulia.rasyid123@gmail.com>
2023-01-13 09:02:50 +0700

    terbaru

--- Fox33/page1.js
+++ Fox33/page1.js
@@ -7,6 +7,20 @@ const user = document.getElementById("user");
 const pass = document.getElementById("pass");
 const btnSignIn = document.getElementById("signIn");
 
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+const btnSignUp = document.getElementById("signUpButton");
+
+btnSignUp.addEventListener("click", function (e) {
+  e.preventDefault();
+  let user = username.value;
+  let pass = password.value;
+  let emailUser = email.value;
+  sign(user, pass, emailUser, akun);
+  // console.log(akun);
+});
+
 btnSignIn.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -65,27 +79,48 @@ function login(userID, Pass, data) {
 
 // console.log(login("user1", "aku123", akun));
 
-// function sign(inputID,inputPass, inputMail, data){
-//     let result=data
-//     for(let perUser of data){
-//         console.log(perUser)
-//         if(inputID===perUser.ID){
-//             return 'id sudah digunakan'
-//             //alert id sudah digunakan
-//         }
-//         if(inputMail === perUser.email){
-//             // alert email sudah digunakan
-//             return 'email sudah digunakan'
-//     }
-//     let dataUser={
-//         ID: inputID,
-//         Password: inputPass,
-//         email: inputMail
-//     }
+function sign(inputID, inputPass, inputMail, data) {
+  //   let result = data;
+  for (let perUser of data) {
+    // console.log(perUser);
+    let isFlagId = false;
+    let isFlagEmail = false;
+    if (inputID === perUser.ID) {
+      // console.log("id sudah ada");
+      isFlagId = true;
+      // break;
+    }
+    if (inputMail === perUser.email) {
+      //   alert email sudah digunakan
+      // console.log("email sudah digunakan");
+      // return "email sudah digunakan";
+      isFlagEmail = true;
+      // break;
+    }
+    if (!isFlagEmail && !isFlagId) {
+      let dataUser = {
+        ID: inputID,
+        Password: inputPass,
+        email: inputMail,
+      };
+      akun.push(dataUser);
+      const elementCustomAlert = document.createElement("div");
+      elementCustomAlert.classList.add("alert");
+      elementCustomAlert.innerText = "Register Berhasil";
 
-//     result.push(dataUser)
-//     }
-//     return result
-// }
+      document.body.insertBefore(elementCustomAlert, document.body.children[0]);
+      setTimeout(() => {
+        elementCustomAlert.remove();
+      }, 2000);
+      break;
+    } else if (isFlagId) {
+      alert("Id sudah digunakan");
+      break;
+    } else if (isFlagEmail) {
+      alert("Email sudah digunakan");
+      break;
+    }
+  }
+}
 
 // console.log(sign('user5','aaa','asdasda',akun))
